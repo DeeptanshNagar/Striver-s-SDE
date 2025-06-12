e4fa2a5770cfc1fe66f2e7a7d86716b50c34cece
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-06-13 01:51:27 +0530

    leet

---
+++ ARRAYS/MaxSubArray/better.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+
+public class better {
+    public static void main(String[] args) {
+        int[] arr = {2,0,0,3}; // note - applicable for ZEROS and NEGATIVES.
+        int k = 3;
+        int len = getLongestSubarray(arr, k);
+        System.out.println("The length of the longest subarray is: " + len);
+    }
+
+    static int getLongestSubarray(int[] arr, int k) {
+        HashMap<Long, Integer> preSumMap = new HashMap<>();
+        int maxlen = 0;
+        long sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            if(sum == k) {
+                maxlen = Math.max(maxlen, i+1);
+            }
+            long remaining = sum - k;
+
+            if(preSumMap.containsKey(remaining)){
+                int len = i - preSumMap.get(remaining);
+                maxlen = Math.max(maxlen, len);
+            }
+            if(!preSumMap.containsKey(sum)){                      // because of this condition.
+                preSumMap.put(sum, i);
+            }
+        }
+        return maxlen;
+    }
+}
--- ARRAYS/MaxSubArray/brute.java
+++ ARRAYS/MaxSubArray/brute.java
@@ -1,12 +1,12 @@
 public class brute {
     public static void main(String[] args) {
         int[] arr = {2, 3, 5, 1, 9};
-        long k = 10;
+        int k = 10;
         int len = getLongestSubarray(arr, k);
         System.out.println("The length of the longest subarray is: " + len);
     }
 
-    static int getLongestSubarray(int[] arr, long k) {
+    static int getLongestSubarray(int[] arr, int k) {
         int n = arr.length;
         int maxlen = 0;
 
