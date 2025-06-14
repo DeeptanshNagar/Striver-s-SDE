08cfadb0fbdd3295ba62e83b24440a9ebc6eadf8
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-06-14 23:53:04 +0530

    dsa

--- ARRAYS/Maximum SubArray/Optimal.java
+++ ARRAYS/Maximum SubArray/Optimal.java
@@ -1,3 +1,5 @@
+// Kadane's algo
+
 public class Optimal {
     public static void main(String[] args) {
         int[] arr = {-2, -3, 4, -1, -2, 1, 5, -3};
@@ -9,9 +11,20 @@ public class Optimal {
     static int maxSubArray(int[] arr) {
         int maxi = Integer.MIN_VALUE;
         int sum = 0;
+        int ansStart = -1;
+        int ansEnd = -1;
+        int Start = 0;
+
         for (int i = 0; i < arr.length; i++) {
+            // condition : sum will be 0 at the start of the array [4 -1 -2 1 5]
+            if(sum == 0) {
+                Start = i;
+            }
             sum += arr[i];
-            maxi = Math.max(maxi, sum);
+            if(sum>maxi){
+                maxi = sum;
+                ansStart = Start; ansEnd = i;
+            }
             if (sum < 0) {
                 sum = 0;
             }
@@ -22,11 +35,16 @@ public class Optimal {
             // }
             
         }
+        System.out.print("The subarray is: [");
+        for (int i = ansStart; i <= ansEnd; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println("]");
         return maxi;
     }
 }
 
-// // Kadane's Algorithm
+// // Kadane's Algorithm (lil wrong version)
 
 // public class better {
 //     public static void main(String[] args) {
