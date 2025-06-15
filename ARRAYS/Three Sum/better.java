8e9b27f4b8e6a18ad0435a5e5d004b572e0a49fb
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-06-16 03:27:14 +0530

    leet

--- ARRAYS/Three Sum/better.java
+++ ARRAYS/Three Sum/better.java
@@ -15,14 +15,20 @@ public class better {
     }
 
     public static List<List<Integer>> threeSum(int[] arr) {
-        
-
-
-
-
-
-
-
-        return new ArrayList<>();
+        int n = arr.length;
+        Set<List<Integer>> st = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            Set<Integer> hashset = new HashSet<>();
+            for (int j = i+1; j < n; j++) {
+                int third = -(arr[i] + arr[j]);
+                if(hashset.contains(third)) {
+                    List<Integer> temp = Arrays.asList(arr[i], arr[j], third);
+                    temp.sort(null);
+                    st.add(temp);
+                }
+                hashset.add(arr[j]);
+            }
+        }
+        return new ArrayList<>(st);
     }
 }
