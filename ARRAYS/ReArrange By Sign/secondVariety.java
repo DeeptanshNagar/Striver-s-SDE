d09c04b10521a0d9cf3b7e1d7e6eceb582dc36bc
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-06-15 23:38:52 +0530

    dsa

---
+++ ARRAYS/ReArrange By Sign/secondVariety.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class secondVariety {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, -4, -5, 3, 4};
+        int[] ans = rearrangeArray(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static int[] rearrangeArray(int[] arr) {
+        int n = arr.length;
+        ArrayList<Integer> positive = new ArrayList<>();
+        ArrayList<Integer> negative = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            if(arr[i] > 0) {
+                positive.add(arr[i]);
+            } else {
+                negative.add(arr[i]);
+            }
+        }
+        if(positive.size() > negative.size()){
+            for (int i = 0; i < negative.size(); i++) {
+                arr[2*i] = positive.get(i);
+                arr[2*i+1] = negative.get(i);
+            }
+
+            int index = negative.size() * 2;
+            for (int i = negative.size(); i < positive.size(); i++) {
+                arr[index] = positive.get(i);
+                index++;
+            }
+        } else {
+            for (int i = 0; i < positive.size(); i++) {
+                arr[2*i] = positive.get(i);
+                arr[2*i+1] = negative.get(i);
+            }
+
+            int index = positive.size() * 2;
+            for (int i = positive.size(); i < negative.size(); i++) {
+                arr[index] = negative.get(i);
+                index++;
+            }
+        }
+        return arr;
+    }
+}
--- ARRAYS/Two Sum/better.java
+++ ARRAYS/Two Sum/better.java
@@ -3,21 +3,21 @@ import java.util.HashMap;
 
 public class better {
     public static void main(String[] args) {
-        int[] nums = {2,7,11,15};
+        int[] nums = {2, 7, 11, 15};
         int target = 9;
-        int [] ans = twoSum(nums, target);
+        int[] ans = twoSum(nums, target);
         System.out.println(Arrays.toString(ans));
     }
+
     static int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> result = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int start = nums[i];
             int more = target - start;
-            if(result.containsKey(more)){
-                return new int[]{i, more};
+            if (result.containsKey(more)) {
+                return new int[]{result.get(more), i};
             }
             result.put(nums[i], i);
-
         }
         return null;
     }
