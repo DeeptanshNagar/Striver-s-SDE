f21a2e7ee8db7a3c964e312dbc2e649da1a8161e
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-07-03 03:40:37 +0530

    dsa

---
+++ ARRAYS/Count Subarray sum Equals K/brute.java
@@ -0,0 +1,23 @@
+public class brute {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, -3, 1, 1, 1, 4, 2, -3};
+        int k = 3;
+        int ans = countSubarray(nums, k);
+        System.out.println(ans);
+    }
+    static int countSubarray(int[] nums, int k) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                int sum = 0;
+                for (int r = i; r <= j; r++) {
+                    sum = sum + nums[r];
+                }
+                if(sum == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
---
+++ ARRAYS/Count Subarray sum Equals K/brute2.java
@@ -0,0 +1,9 @@
+public class brute2 {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, -3, 1, 1, 1, 4, 2, -3};
+        int k = 3;
+        int ans = countSubarray(nums, k);
+        System.out.println(ans);
+    }
+    static int countSubarray(int[] nums)
+}
