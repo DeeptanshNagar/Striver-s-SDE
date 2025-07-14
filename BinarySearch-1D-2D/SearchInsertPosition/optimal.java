120b3323468a08054418541f63dc6de9a5c6eb6e
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-07-14 21:09:01 +0530

    dsa

---
+++ BinarySearch-1D-2D/SearchInsertPosition/optimal.java
@@ -0,0 +1,26 @@
+public class optimal {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 7};
+        int x = 6;
+        int ind = searchInsert(arr, x);
+        System.out.println("The index is: " + ind);
+    }
+    public static int searchInsert(int [] arr, int x) {
+        int n = arr.length;
+        int low = 0, high = n - 1;
+        int ans = n;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            // potential answer found
+            if (arr[mid] >= x) {
+                ans = mid;
+                //look for smaller index on the left
+                high = mid - 1;
+            } else {
+                low = mid + 1; // look on the right
+            }
+        }
+        return ans;
+    }
+}
