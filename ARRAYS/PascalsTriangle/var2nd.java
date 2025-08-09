69cdd9fde2e56ba7f504cf2d38ecce07b6923c65
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-08-09 08:27:40 +0530

    dsa

---
+++ ARRAYS/PascalsTriangle/var2nd.java
@@ -0,0 +1,15 @@
+
+import java.util.Arrays;
+
+public class var2nd {
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(Arrays.toString(printRow(n)));
+    }
+
+    static int printRow(int N) {
+        for (int c = 1; c < N; c++) {
+            printRoww()
+        }
+    }
+}
