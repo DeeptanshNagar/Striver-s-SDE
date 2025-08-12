190a94bfd91214ca97a5d82c912781d97e45fe9f
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-08-12 19:52:10 +0530

    dsa

--- TUFrevision/Arrays/MaxSubarr.java
+++ TUFrevision/Arrays/MaxSubarr.java
@@ -1,24 +1,24 @@
 // BruteForce solution with time complexity order of N square.
 
-// public class MaxSubarr {
-//     public static void main(String[] args) {
-//         int[] arr ={-2, -3, 4, -1, -2, 1, 5, -3};
-//         int ans = maxiSubarray(arr);
-//         System.out.println(ans);
-//     }
+public class MaxSubarr {
+    public static void main(String[] args) {
+        int[] arr ={-2, -3, 4, -1, -2, 1, 5, -3};
+        int ans = maxiSubarray(arr);
+        System.out.println(ans);
+    }
 
-//     static int maxiSubarray(int[] arr) {
-//         int maxi = 0;
-//         for (int i = 0; i < arr.length; i++) {
-//             int sum = 0;
-//             for (int j = i; j < arr.length; j++) {
-//                 sum = sum + arr[j];
-//                 maxi = Math.max(maxi, sum);
-//             }
-//         }
-//         return maxi;
-//     }
-// }
+    static int maxiSubarray(int[] arr) {
+        int maxi = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+            for (int j = i; j < arr.length; j++) {
+                sum = sum + arr[j];
+                maxi = Math.max(maxi, sum);
+            }
+        }
+        return maxi;
+    }
+}
 
 
 
