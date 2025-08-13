7f7f67cd70cd0c9a04bd8afb72ad3d1f7fbb13cf
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-08-14 00:42:19 +0530

    dnF

--- TUFrevision/Arrays/DutchNationalFlag.java
+++ TUFrevision/Arrays/DutchNationalFlag.java
@@ -1,5 +1,12 @@
+// Popular Algo - National Dutch Flag Algo.
+
+// [0 to low - 1] {extreme left}
+// [low to mid - 1] {. .}
+// [mid + 1 to n - 1] {extreme right}
+
 public class DutchNationalFlag {
     public static void main(String[] args) {
         
     }
+    
 }
