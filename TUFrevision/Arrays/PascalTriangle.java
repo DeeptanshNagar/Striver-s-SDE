58a93a0070c6a81e9fa52aa9e6ef727da4a9e076
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-08-13 23:35:25 +0530

    pt

--- TUFrevision/Arrays/PascalTriangle.java
+++ TUFrevision/Arrays/PascalTriangle.java
@@ -1,5 +1,25 @@
 public class PascalTriangle {
     public static void main(String[] args) {
-        
+        int row = 5;
+        int col = 3;
+        int element = PascalTriangle(row, col);
+        System.out.println(element);
     }
+
+    static int PascalTriangle(int row, int col) {
+        int element = (int) ncr(row - 1, col - 1);
+        return element;
+    }
+
+    static long ncr(int n, int r) {
+        long res = 1;
+        for (int i = 0; i < r; i++) {
+            res = res * (n - i);
+            res = res / (i + 1);
+        }
+        
+        return res;
+    }
+    
+
 }
