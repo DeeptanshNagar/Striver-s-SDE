ae09d4b479fe63c9195af5c1f462d2f17afacc31
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-08-19 22:00:49 +0530

    np

--- ARRAYS/NextPermutation/Optimized.java
+++ ARRAYS/NextPermutation/Optimized.java
@@ -1,17 +1,8 @@
 import java.util.Arrays;
 
-// So the intuition is - 
-// Find the break-point, i: Break-point means the first index i from the back of the given array where arr[i] becomes smaller than arr[i+1].
-// For example, if the given array is {2,1,5,4,3,0,0}, the break-point will be index 1(0-based indexing). Here from the back of the array, index 1 is the first index where arr[1] i.e. 1 is smaller than arr[i+1] i.e. 5.
-// To find the break-point, using a loop we will traverse the array backward and store the index i where arr[i] is less than the value at index (i+1) i.e. arr[i+1].
-// If such a break-point does not exist i.e. if the array is sorted in decreasing order, the given permutation is the last one in the sorted order of all possible permutations. So, the next permutation must be the first i.e. the permutation in increasing order.
-// So, in this case, we will reverse the whole array and will return it as our answer.
-// If a break-point exists:
-// Find the smallest number i.e. > arr[i] and in the right half of index i(i.e. from index i+1 to n-1) and swap it with arr[i].
-// Reverse the entire right half(i.e. from index i+1 to n-1) of index i. And finally, return the array.
 public class Optimized {
     public static void main(String[] args) {
-        int[] nums = {1,1,5};
+        int[] nums = {2, 1, 5, 4, 3, 0, 0};
         nextPermutation(nums);
         System.out.println(Arrays.toString(nums));
     }
@@ -19,27 +10,31 @@ public class Optimized {
     public static void nextPermutation(int[] nums) {
         int n = nums.length;
         int pivot = -1;
-        for (int i = n-2; i >= 0; i--) {
-            if(nums[i] < nums[i+1]) {
+
+        // Step 1: Find pivot
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
                 pivot = i;
                 break;
             }
         }
 
-        if(pivot == -1) {
-            reverse(nums, 0, n-1);
+        // Step 2: If no pivot, reverse whole array
+        if (pivot == -1) {
+            reverse(nums, 0, n - 1);
+            return;
         }
 
-        for (int i = n-1; i > pivot; i--) {
-            if(nums[i] > nums[pivot]) {
+        // Step 3: Find rightmost element greater than pivot
+        for (int i = n - 1; i > pivot; i--) {
+            if (nums[i] > nums[pivot]) {
                 swap(nums, i, pivot);
                 break;
             }
         }
-        for (int i = pivot + 1; i < nums.length; i++) {
-            reverse(nums, pivot+1, nums.length-1);    
-        }
 
+        // Step 4: Reverse suffix (once, not in a loop)
+        reverse(nums, pivot + 1, n - 1);
     }
 
     public static void swap(int[] nums, int i, int j) {
@@ -53,7 +48,6 @@ public class Optimized {
             int temp = nums[left];
             nums[left] = nums[right];
             nums[right] = temp;
-
             left++;
             right--;
         }
