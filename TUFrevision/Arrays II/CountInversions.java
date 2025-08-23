928b439b95124ea8109ce6fc6ae6a77fce17f841
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-08-23 23:49:07 +0530

    ci

--- TUFrevision/Arrays II/CountInversions.java
+++ TUFrevision/Arrays II/CountInversions.java
@@ -4,4 +4,16 @@ public class CountInversions {
         int res = totalInversions(arr);
         System.out.println(res);
     }
+
+    static int totalInversions(int[] arr) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if(arr[i] > arr[j]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 }
