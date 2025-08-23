a172a75582c0cdece27bb5549c6c4363e98333d4
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-08-23 13:47:24 +0530

    source control

--- ARRAYS/FindDuplicateInN+1Integers/brute.java
+++ ARRAYS/FindDuplicateInN+1Integers/brute.java
@@ -1,11 +1,11 @@
 public class brute {
     public static void main(String args[]) {
-        int arr[] = {1,3,4,2,3};
+        int[] arr = {1, 3, 4, 2, 6, 7, 8, 9, 2};
         System.out.println("The duplicate element is " + findDuplicate(arr));
     }
     static int findDuplicate(int[] arr) {
         int n = arr.length;
-        int[] freq = new int[n + 1]; // [0, 0, 0, 0, 0, 0]
+        int[] freq = new int[n + 1];   // [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         for (int i = 0; i < n; i++) {
             if (freq[arr[i]] == 0) {
                 freq[arr[i]] += 1;
