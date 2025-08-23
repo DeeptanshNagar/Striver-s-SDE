883b0a98d3ce614d6c022b05020d5721b1de12db
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-08-24 00:00:18 +0530

    2d

--- TUFrevision/Arrays II/search2Dmatrix.java
+++ TUFrevision/Arrays II/search2Dmatrix.java
@@ -1,5 +1,19 @@
 public class search2Dmatrix {
     public static void main(String[] args) {
-        
+        int[][] matrix = {
+            {3, 4, 7, 9},
+            {12, 13, 16, 18},
+            {20, 21, 23, 29},
+            {32, 35, 47, 52}
+        };
+        int n = matrix.length;
+        int m = matrix[0].length;
+        boolean result = searchMatrix(matrix, n, m);
+        System.out.println(result);
     }
+
+    static boolean searchMatrix(int[][] matrix, int n, int m) {
+        
+        return false;
+    } 
 }
