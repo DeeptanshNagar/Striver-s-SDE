b09c3ffd8d96b2c9129030c2ec74d811ab657f86
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-09-16 02:07:55 +0530

    ds

--- Strings/Leetcode1935/Solution.java
+++ Strings/Leetcode1935/Solution.java
@@ -16,12 +16,12 @@ public class Solution {
     }
 
     public int canBeTypedWords(String text, String brokenLetters) {
-        String[] words = text.split(" ");
+        String[] words = text.split(" "); // ["hello", "world"]
         int count = 0;
 
         for (String word : words) {
             boolean canType = true;
-            for (char ch : brokenLetters.toCharArray()) {
+            for (char ch : brokenLetters.toCharArray()) { // ['a','d']
                 if (word.indexOf(ch) != -1) {
                     canType = false;
                     break;
